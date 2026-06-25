8b482a5f23153c1141b91b0f8c90280d0a640272
abhijith-account <abhijithp.student@gmail.com>
2026-06-25 13:26:27 +0530

    Initial code

--- src/QEMU_Startup_Probe.c
+++ src/QEMU_Startup_Probe.c
@@ -14,94 +14,40 @@ static void qemu_write0(const char *s)
     );
 }
 
-static int qemu_probe_pre_kernel_1_p0(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P0\n");
-    return 0;
-}
+#define MAKE_PROBE(name, text)        \
+    static int name(void)             \
+    {                                 \
+        qemu_write0(text "\n");       \
+        return 0;                     \
+    }
 
-static int qemu_probe_pre_kernel_1_p10(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P10\n");
-    return 0;
-}
+MAKE_PROBE(qemu_probe_pre_kernel_1_p0,  "QEMU_PROBE_PRE_KERNEL_1_P0")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p1,  "QEMU_PROBE_PRE_KERNEL_1_P1")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p2,  "QEMU_PROBE_PRE_KERNEL_1_P2")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p3,  "QEMU_PROBE_PRE_KERNEL_1_P3")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p4,  "QEMU_PROBE_PRE_KERNEL_1_P4")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p5,  "QEMU_PROBE_PRE_KERNEL_1_P5")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p6,  "QEMU_PROBE_PRE_KERNEL_1_P6")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p7,  "QEMU_PROBE_PRE_KERNEL_1_P7")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p8,  "QEMU_PROBE_PRE_KERNEL_1_P8")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p9,  "QEMU_PROBE_PRE_KERNEL_1_P9")
+MAKE_PROBE(qemu_probe_pre_kernel_1_p10, "QEMU_PROBE_PRE_KERNEL_1_P10")
 
-static int qemu_probe_pre_kernel_1_p20(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P20\n");
-    return 0;
-}
+MAKE_PROBE(qemu_probe_pre_kernel_2_p0,  "QEMU_PROBE_PRE_KERNEL_2_P0")
+MAKE_PROBE(qemu_probe_post_kernel_p0,   "QEMU_PROBE_POST_KERNEL_P0")
+MAKE_PROBE(qemu_probe_application_p0,   "QEMU_PROBE_APPLICATION_P0")
 
-static int qemu_probe_pre_kernel_1_p30(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P30\n");
-    return 0;
-}
-
-static int qemu_probe_pre_kernel_1_p40(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P40\n");
-    return 0;
-}
-
-static int qemu_probe_pre_kernel_1_p50(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P50\n");
-    return 0;
-}
-
-static int qemu_probe_pre_kernel_1_p60(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P60\n");
-    return 0;
-}
-
-static int qemu_probe_pre_kernel_1_p70(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P70\n");
-    return 0;
-}
-
-static int qemu_probe_pre_kernel_1_p80(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P80\n");
-    return 0;
-}
-
-static int qemu_probe_pre_kernel_1_p90(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_1_P90\n");
-    return 0;
-}
-
-static int qemu_probe_pre_kernel_2_p0(void)
-{
-    qemu_write0("QEMU_PROBE_PRE_KERNEL_2_P0\n");
-    return 0;
-}
-
-static int qemu_probe_post_kernel_p0(void)
-{
-    qemu_write0("QEMU_PROBE_POST_KERNEL_P0\n");
-    return 0;
-}
-
-static int qemu_probe_application_p0(void)
-{
-    qemu_write0("QEMU_PROBE_APPLICATION_P0\n");
-    return 0;
-}
-
-SYS_INIT(qemu_probe_pre_kernel_1_p0, PRE_KERNEL_1, 0);
+SYS_INIT(qemu_probe_pre_kernel_1_p0,  PRE_KERNEL_1, 0);
+SYS_INIT(qemu_probe_pre_kernel_1_p1,  PRE_KERNEL_1, 1);
+SYS_INIT(qemu_probe_pre_kernel_1_p2,  PRE_KERNEL_1, 2);
+SYS_INIT(qemu_probe_pre_kernel_1_p3,  PRE_KERNEL_1, 3);
+SYS_INIT(qemu_probe_pre_kernel_1_p4,  PRE_KERNEL_1, 4);
+SYS_INIT(qemu_probe_pre_kernel_1_p5,  PRE_KERNEL_1, 5);
+SYS_INIT(qemu_probe_pre_kernel_1_p6,  PRE_KERNEL_1, 6);
+SYS_INIT(qemu_probe_pre_kernel_1_p7,  PRE_KERNEL_1, 7);
+SYS_INIT(qemu_probe_pre_kernel_1_p8,  PRE_KERNEL_1, 8);
+SYS_INIT(qemu_probe_pre_kernel_1_p9,  PRE_KERNEL_1, 9);
 SYS_INIT(qemu_probe_pre_kernel_1_p10, PRE_KERNEL_1, 10);
-SYS_INIT(qemu_probe_pre_kernel_1_p20, PRE_KERNEL_1, 20);
-SYS_INIT(qemu_probe_pre_kernel_1_p30, PRE_KERNEL_1, 30);
-SYS_INIT(qemu_probe_pre_kernel_1_p40, PRE_KERNEL_1, 40);
-SYS_INIT(qemu_probe_pre_kernel_1_p50, PRE_KERNEL_1, 50);
-SYS_INIT(qemu_probe_pre_kernel_1_p60, PRE_KERNEL_1, 60);
-SYS_INIT(qemu_probe_pre_kernel_1_p70, PRE_KERNEL_1, 70);
-SYS_INIT(qemu_probe_pre_kernel_1_p80, PRE_KERNEL_1, 80);
-SYS_INIT(qemu_probe_pre_kernel_1_p90, PRE_KERNEL_1, 90);
 
 SYS_INIT(qemu_probe_pre_kernel_2_p0, PRE_KERNEL_2, 0);
 SYS_INIT(qemu_probe_post_kernel_p0, POST_KERNEL, 0);
