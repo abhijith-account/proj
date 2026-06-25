d50e10ab7661e56eec8dd37e221f7e756a170bba
abhijith-account <abhijithp.student@gmail.com>
2026-06-25 12:12:47 +0530

    Initial code

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,13 @@
 
 #include "RTOS_Command_based_thread_system.h"
 #include "RTOS_Synchronization_Layer.h"
+#include <new>
 #include "Device_State_Machine+Watchdog.h"
 #include "Smart_Battery_System.h"
 #include "Persistent_Configuration_System.h"
 #include "Static_Memory+MISRA_Compliance_Layer.h"
-#include "Power_Management_System.h"
-
 #include <zephyr/debug/thread_analyzer.h>
-#include <new>
+#include "Power_Management_System.h"
 
 LOG_MODULE_REGISTER(MAIN_OS, LOG_LEVEL_INF);
 
@@ -19,15 +18,46 @@ DeviceContext sys_context;
 
 extern ZephyrWorkQueue status_work;
 
+/*
+ * QEMU semihost marker for CI only.
+ * This does not use Zephyr semihost_poll_out(), so there is no linker issue.
+ * It prints through QEMU when the workflow passes -semihosting-config enable=on,target=native.
+ */
+#if defined(QEMU_RUNTIME_PROBE)
+static void qemu_write0(const char *s)
+{
+    register int op asm("r0") = 0x04;
+    register const char *msg asm("r1") = s;
+
+    asm volatile (
+        "bkpt 0xab"
+        :
+        : "r"(op), "r"(msg)
+        : "memory"
+    );
+}
+#else
+static void qemu_write0(const char * /* s */)
+{
+}
+#endif
+
 int main(void)
 {
-    printk("QEMU_BOOT_MARKER\n");
+    qemu_write0("QEMU_BOOT_MARKER_ACTUAL_MAIN\n");
+    printk("QEMU_BOOT_MARKER_PRINTK\n");
+
     LOG_INF("Command-Based RTOS Booting");
+    qemu_write0("MAIN_AFTER_LOG_INF\n");
 
     ConfigStore& config = ConfigStore::getInstance();
+    qemu_write0("MAIN_BEFORE_CONFIG_INIT\n");
 
     if (config.init()) {
+        qemu_write0("MAIN_AFTER_CONFIG_INIT\n");
+
         config.validateEndurance(ConfigKey::ALARM_THRESHOLD);
+        qemu_write0("MAIN_AFTER_ENDURANCE_TEST\n");
 
         uint16_t infusion_rate = 0;
 
@@ -40,7 +70,10 @@ int main(void)
     }
 
     sys_context.requestTransition(SystemState::RUNNING);
+    qemu_write0("MAIN_AFTER_STATE_RUNNING\n");
+
     status_work.schedule(K_SECONDS(1));
+    qemu_write0("MAIN_AFTER_STATUS_WORK_SCHEDULE\n");
 
     return 0;
 }
